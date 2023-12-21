9648f87aa7f8e7b05fdf67facdbb4a6036ed8978
Noah Frank <64226459+noahf2020@users.noreply.github.com>
2023-12-21 08:46:34 -0500

    Init

--- src/main/java/com/example/tableview/Activity.java
+++ src/main/java/com/example/tableview/Activity.java
@@ -1,6 +1,18 @@
 package com.example.tableview;
 
 public class Activity {
+    private String Name;
+    private int Grade;
+    private int Sports;
+    private int GPA;
+    private int Standing;
 
+    public Activity(String Name, int Grade, int Sports, int GPA, int Standing){
+        this.Name = Name;
+        this.Grade = Grade;
+        this.Sports = Sports;
+        this.GPA = GPA;
+        this.Standing = Standing;
+    }
 
 }
